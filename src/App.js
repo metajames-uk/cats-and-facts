3fa44a9da6b17ff7102777eb48f42363064cf495
James Hollands <james.hollands@pushdr.com>
2019-12-19 18:13:24 +0000

    got the basic bit of the app working

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useState } from "react";
+import "./App.css";
+import { Navbar } from "./components/layout/Navbar";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { About } from "./components/pages/About";
 
-function App() {
+const App = () => {
+  const [randomFact, setRandomFact] = useState({
+    fact: "",
+    length: 0
+  });
+  const getRandomFact = async () => {
+    const res = await fetch("https://catfact.ninja/fact");
+    const json = await res.json();
+    setRandomFact({
+      fact: json.fact,
+      length: json.length
+    });
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Fragment>
+          <header>
+            <h1>Facts of cats</h1>
+            <Navbar />
+          </header>
+          <div className="container">
+            <Switch>
+              <Route exact path="/">
+                <div className="RandomGenerateFact">
+                  <button onClick={getRandomFact}>
+                    Generate a fact about cats
+                  </button>
+                </div>
+                <div className="Random Fact">
+                  {randomFact.length !== 0 ? (
+                    <div>
+                      <h3>Random Cat Fact</h3>
+                      <p>{randomFact.fact}</p>
+                    </div>
+                  ) : (
+                    <div></div>
+                  )}
+                </div>
+              </Route>
+              <Route exact path="/about">
+                <About></About>
+              </Route>
+            </Switch>
+          </div>
+        </Fragment>
+      </Router>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/pages/About.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export const About = () => {
+  return (
+    <div>
+      <h2>About page</h2>
+      <p>
+        This is a react app using the{" "}
+        <a
+          href="https://apilist.fun/api/cat-facts"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Cat Facts API
+        </a>
+      </p>
+    </div>
+  );
+};
